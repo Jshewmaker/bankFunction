04f45886278b8966470e1ce5e482aef2d87a2b4f
Joshua Shewmaker <joshuashewmaker@Joshuas-MBP.gha.chartermi.net>
2015-02-24 15:59:50 -0500

    Initial Commit

---
+++ bankFunction/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  bankFunction
+//
+//  Created by Joshua Shewmaker on 2/24/15.
+//  Copyright (c) 2015 Joshua Shewmaker. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
